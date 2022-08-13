aa9500c1ab64f4f21fbff884f5f0effbff2cfa70
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2022-08-14 03:13:57 +0530

    LC

---
+++ 1094. Car Pooling.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int stops[1001];              // Since number of stops cannot be more than 1001
+        for(auto trip: trips){
+            stops[trip[1]] += trip[0];
+            stops[trip[2]] -= trip[0];
+        }
+        
+        for(int i=0;i<1001 && capacity > 0;i++)
+            capacity -= stops[i];
+        return capacity >= 0;
+    }
+};
