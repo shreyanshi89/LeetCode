4c3f2eae1006212b8c39a85f95aa8f3ce43f095e
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2022-08-25 12:50:28 +0530

    LC

---
+++ 383. Ransom Note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char, int> m1;
+        map<char, int> m2;
+        
+        for(int i=0;i<magazine.length();i++)
+            m1[magazine[i]]++;
+        
+        for(int i=0;i<ransomNote.length();i++)
+            m2[ransomNote[i]]++;
+        
+        for(int i=0;i<ransomNote.length();i++){
+            auto c1 = m1.find(ransomNote[i]);
+            auto c2 = m2.find(ransomNote[i]);
+            if(c1->second < c2->second)
+                return false;
+        }
+        return true;
+    }
+};
