aafd86ae4c48053c9982eebd9861547960c398ed
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2022-08-30 20:20:30 +0530

    LC

---
+++ 48. Rotate Image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<i;j++)
+                swap(matrix[i][j],matrix[j][i]);
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n/2;j++)
+                swap(matrix[i][j],matrix[i][n-1-j]);
+        }
+    }
+};
