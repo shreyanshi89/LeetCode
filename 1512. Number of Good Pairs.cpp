5848cbb0ffea5ba456fc49efad720014d1f0d491
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2022-09-11 20:44:59 +0530

    LC

---
+++ 1512. Number of Good Pairs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int res = 0;
+        unordered_map<int, int> m;
+        for(int num: nums)
+            res += m[num]++;
+        return res;
+    }
+};
