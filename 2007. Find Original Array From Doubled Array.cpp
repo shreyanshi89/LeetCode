389688447cb4663d26f5642d6e308f17c006d733
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2022-09-15 17:59:13 +0530

    LC

--- 2007. Find Original Array From Doubled Array.cpp
+++ 2007. Find Original Array From Doubled Array.cpp
@@ -39,3 +39,39 @@ public:
         return ans;
     }
 };
+
+// OR
+/*
+Simplified: 
+
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        vector<int> ans;
+        sort(changed.begin(),changed.end());
+        unordered_map<int, int> m;
+        
+        if(changed.size() % 2 != 0)
+            return {};
+        
+        for(int num: changed)
+            m[num]++;
+        
+        for(int num: changed){
+            auto l = m.find(num);
+            auto it = m.find(2 * num);
+            
+            if(l->second == 0)
+                continue;
+            
+            else if(it == m.end() || l->second > it->second)
+                return {};
+          
+            ans.push_back(num);
+            it->second -=1;
+            l->second -= 1;
+        }
+        return ans;
+    }
+};
+*/
